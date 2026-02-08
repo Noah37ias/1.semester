2df03c54bf5b83e1d0f1cacd318ec5d450ec64a7
Noah37ias <Noah37ias@gmail.com>
2026-02-08 20:19:58 +0100

    .

--- Lek04/src/opg3/alder.java
+++ Lek04/src/opg3/alder.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class alder {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Insert age here: ");
+        System.out.print("Insert age here: ");
         int age = input.nextInt();
         alder tjekAlder = new alder();
         String result = tjekAlder.institution(age);
         System.out.println(result);
-
+        System.out.print("Type true if Girl, and false if boy: ");
+        boolean isGirl = input.nextBoolean();
+        alder tjekHold = new alder();
+        String resultHold = tjekHold.team(isGirl,age);
+        System.out.println(resultHold);
     }
     public String institution(int age){
         if(age > 0 && age <=2){
@@ -24,5 +28,20 @@ public class alder {
             return "School";
         }
         else return "Out of School";
+    }
+    public String team(boolean isGirl, int age){
+        if(isGirl==true){
+            if(age>=8){
+                return "Springy girls";
+            }
+            else{
+                return "Tumbling girls";
+            }
+        }
+        if(isGirl==false){
+            if(age>=8) return "Cool boys";
+            else return "Young cubs";
+        }
+        return null;
     }
 }
---
+++ Lek04/src/opg4/rainbow.java
@@ -0,0 +1,12 @@
+package opg4;
+
+import java.util.Random;
+
+public class rainbow {
+    public static void main(String[]args){
+        int n = (int)(Math.random()*7);
+        n++;
+        System.out.println(n);
+        
+    }
+}
