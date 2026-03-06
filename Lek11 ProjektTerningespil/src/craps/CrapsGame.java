04c4a2cb62b84c70681d7b60902081be59834662
noah37ias <noah37ias@gmail.com>
2026-03-06 09:06:44 +0100

    Update 06-03-2026

--- Lek11 ProjektTerningespil/src/craps/CrapsGame.java
+++ Lek11 ProjektTerningespil/src/craps/CrapsGame.java
@@ -1,23 +1,32 @@
 package craps;
 
 public class CrapsGame {
-    void main(){
-    Player p1 = new Player();
-    p1.play();
-    printResults(p1);
+    void main() {
+        printRules();
+        Player p1 = new Player();
+        p1.play();
+        printResults(p1);
 
     }
+
     public static void printRules() {
-        IO.println("=====================================================");
-        IO.println("Rules of RollOneDie:");
-        IO.println("The player throws one die as long as he/she wants.");
-        IO.println("=====================================================");
+        IO.println("Rules of Craps");
+        IO.println("* First roll:");
+        IO.println("-----------------------------------------------------------------------");
+        IO.println("7 or 11             -   You win!");
+        IO.println("2,3 or 12           -   You lose!");
+        IO.println("4,5,6,8,9 or 10     -   Point set, the game continues!");
+        IO.println("-----------------------------------------------------------------------");
+        IO.println("* When point is set, you keep rolling the dice until:");
+        IO.println("Hit your set point  -   You win!");
+        IO.println("7                   -   You lose!");
+        IO.println("-----------------------------------------------------------------------");
     }
 
     public static void printResults(Player p1) {
         IO.println("Results");
         IO.println("-------");
-        IO.println("Roll count: "+ p1.getRollCount());
+        IO.println("Roll count: " + p1.getRollCount());
 
     }
 }
---
+++ Lek11 ProjektTerningespil/src/pigExtended2/Player.java
@@ -0,0 +1,59 @@
+package pigExtended2;
+
+public class Player {
+    private Die die1;
+    private int rollCount;
+    private int points;
+
+    private int roundNumber;
+
+    public Player() {
+        this.die1 = new Die();
+    }
+
+    public void throwDie() {
+        die1.roll();
+        rollCount++;
+    }
+
+    public int getRollCount() {
+        return rollCount;
+    }
+    public int getPoints(){
+        return points;
+    }
+    public int getRoundNumber() {
+        return roundNumber;
+    }
+    public void playTurn() {
+        int roundPoints = 0;
+
+        IO.print("Roll? ");
+        IO.readln();
+        boolean finished = false;
+        while (!finished) {
+
+            throwDie();
+            IO.println(String.format("Rolling... %d", die1.getFaceValue()));
+
+            if (die1.getFaceValue() == 1) {
+                IO.println("Your round is over, total points now: " + points );
+                roundNumber++;
+                finished = true;
+
+            } else {
+                roundPoints+= die1.getFaceValue();
+                IO.println("Points this round: " + roundPoints + " Total points now: " + points );
+                IO.println("Roll again? (Y/n)");
+                String again = IO.readln();
+                if (again.equalsIgnoreCase("n")) {
+                    points += roundPoints;
+                    IO.println("Your round is over, total points now: " + points );
+                    IO.println();
+                    roundNumber++;
+                    finished = true;
+                }
+            }
+        }
+    }
+}
