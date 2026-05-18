8a57f31e78430f860988173df9a236dd5992f1ca
Noah37ias <Noah37ias@gmail.com>
2026-05-18 14:32:28 +0200

    Update 18-05-2026 :)
    Signed-off-by: Noah37ias <Noah37ias@gmail.com>

--- Lek26 Fletning af sekvenser/src/opg3/AllCustomers.java
+++ Lek26 Fletning af sekvenser/src/opg3/AllCustomers.java
@@ -2,21 +2,23 @@ package opg3;
 
 import opg1.Customer;
 
+import java.security.cert.Certificate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class AllCustomers {
     void main() {
-        Customer c1 = new Customer("Noah","Noah");
-        Customer c2 = new Customer("Carsten","sdw");
-        Customer c3 = new Customer("Arian","asfw");
-        Customer c4 = new Customer("Arian","oifio");
-        Customer c5 = new Customer("Arian","sfkew");
-        Customer c6 = new Customer("Arian","gaks");
-        Customer c7 = new Customer("Noah","Noah");
-        Customer c8 = new Customer("Arian","asfw");
+        Customer c1 = new Customer("Noah", "Noah");
+        Customer c2 = new Customer("Carsten", "sdw");
+        Customer c3 = new Customer("Arian", "asfw");
+        Customer c4 = new Customer("Arian", "oifio");
+        Customer c5 = new Customer("Arian", "sfkew");
+        Customer c6 = new Customer("Arian", "gaks");
+        Customer c7 = new Customer("Noah", "Noah");
+        Customer c8 = new Customer("Arian", "asfw");
         ArrayList<Customer> liste1 = new ArrayList<>();
-        Customer[] liste2 = {c6,c7,c4,c5};
+        Customer[] liste2 = {c6, c7, c4, c5};
 
         liste1.add(c1);
         liste1.add(c2);
@@ -27,7 +29,8 @@ public class AllCustomers {
 //        liste2.add(c7);
         liste1.add(c8);
         Collections.sort(liste1);
-        IO.println(goodCustomers(liste1,liste2));
+        Arrays.sort(liste2);
+        IO.println(goodCustomers(liste1, liste2));
     }
 
     public ArrayList<Customer> goodCustomers(
@@ -38,19 +41,21 @@ public class AllCustomers {
         int i2 = 0;
 
         while (i1 < l1.size() && i2 < l2.length) {
-            if (l1.get(i1).equals(l2[i2])) {
-                i1++;
-                i2++;
-            }
-            else if(l1.get(i1).compareTo(l2[i2])<0){
+            if (l1.get(i1).compareTo(l2[i2]) < 0) {
                 result.add(l1.get(i1));
                 i1++;
-        }
-            else{
-                result.add(l2[i2]);
+            } else if (l1.get(i1).compareTo(l2[i2]) > 0) {
+                i2++;
+            } else {
+                i1++;
                 i2++;
             }
-    }
+
+        }
+        while (i1 < l1.size()) {
+            result.add(l1.get(i1));
+            i1++;
+        }
         return result;
-}
+    }
 }
