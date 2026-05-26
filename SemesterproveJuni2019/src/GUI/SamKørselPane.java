14bd216a576c7572f98b8b254ca3e7751aeb9671
noah37ias <noah37ias@gmail.com>
2026-05-26 18:15:57 +0200

    Update 18-05-2026
    Signed-off-by: noah37ias <noah37ias@gmail.com>

---
+++ SemesterproveJuni2019/src/GUI/SamKørselPane.java
@@ -0,0 +1,164 @@
+package GUI;
+
+import controller.Controller;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import model.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javafx.scene.control.*;
+
+
+public class SamKørselPane extends Application {
+    private DatePicker datoValg;
+    private Button visLift;
+    private ListView<Lift> lifts;
+    private TextArea taNavn;
+    private TextArea taSted;
+    private TextArea taTid;
+    private Button btnOpret;
+    private ListView<Booking> lswBookinger;
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Samkørsel");
+        GridPane pane = new GridPane();
+        pane.setStyle("-fx-background-color: #93c47e;");
+        this.initContent(pane);
+
+        //SCROLLBAR
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(pane);
+        scrollPane.setFitToWidth(true);
+        //Opretter vores stage samt størrelse
+        Scene scene = new Scene(scrollPane, 740, 470);
+        stage.setScene(scene);
+        //Viser vores stage
+        stage.show();
+
+    }
+
+    // -------------------------------------------------------------------------
+
+    private void initContent(GridPane pane) {
+        //Mellemrum mellem ting
+        pane.setPadding(new Insets(20));
+        pane.setHgap(10);
+        pane.setVgap(15);
+
+        VBox venstre = del1();
+        VBox højre = del2();
+        højre.setMaxWidth(300);
+        pane.add(venstre, 0, 0);
+        pane.add(højre, 1, 0);
+
+    }
+
+    private HBox datooglift() {
+        HBox boks = new HBox(10);
+        Label dato = new Label("Udbudte lift på dato: ");
+        datoValg = new DatePicker();
+        visLift = new Button("Vis lift");
+
+        boks.getChildren().addAll(dato, datoValg, visLift);
+        return boks;
+    }
+
+    private VBox del1() {
+        VBox boks = new VBox(10);
+        HBox h1 = datooglift();
+
+        lifts = new ListView<>();
+        visLift.setOnAction(event -> {
+            if (datoValg != null) {
+                lifts.getItems().setAll(Controller.liftPåDato(datoValg.getValue()));
+            }
+        });
+        lifts.getSelectionModel().selectedItemProperty().addListener((_, _, _) -> {
+            opdaterBookinger();
+        });
+        boks.getChildren().addAll(h1, lifts);
+
+        return boks;
+    }
+
+    public VBox del2() {
+        VBox boks = new VBox(10);
+        Label passager = new Label("Passager: ");
+        Label sted = new Label("Opsamlingssted: ");
+        Label tid = new Label("Opsamlings tid: ");
+        taNavn = new TextArea();
+        taSted = new TextArea();
+        taTid = new TextArea();
+        taTid.setMaxHeight(8);
+        taNavn.setMaxHeight(8);
+        taSted.setMaxHeight(8);
+        btnOpret = new Button("Opret booking");
+        Label b1 = new Label("Bookinger");
+        lswBookinger = new ListView<>();
+        lswBookinger.setMaxHeight(145);
+        btnOpret.setOnAction(event -> opretBooking());
+        //lifts.getSelectionModel().clearSelection();
+
+        boks.getChildren().addAll(passager, taNavn, sted, taSted, tid, taTid, btnOpret, b1, lswBookinger);
+
+        return boks;
+    }
+
+    public void opretBooking() {
+        if (taNavn.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Mangler navn");
+            alert.setContentText("Du skal vælge et navn før du kan tilmelde dig");
+            alert.showAndWait();
+        } else if (taSted.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Mangler sted");
+            alert.setContentText("Du skal vælge et sted før du kan tilmelde dig");
+            alert.showAndWait();
+        } else if (taTid.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Mangler tid");
+            alert.setContentText("Du skal vælge et tidpunkt før du kan tilmelde dig");
+            alert.showAndWait();
+        } else {
+            Lift lift = lifts.getSelectionModel().getSelectedItem();
+            if (!lift.LedigPlads()) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Ingen pladser");
+                alert.setContentText("Der er ikke flere pladser");
+                alert.showAndWait();
+            } else {
+                String navn = taNavn.getText();
+                LocalTime tid = LocalTime.parse(taTid.getText());
+                String sted = taSted.getText();
+
+                Controller.createBooking(navn, tid, sted, lift);
+                taNavn.clear();
+                taSted.clear();
+                taTid.clear();
+                opdaterBookinger();
+            }
+        }
+    }
+
+    public void opdaterBookinger() {
+        Lift lift = lifts.getSelectionModel().getSelectedItem();
+        lswBookinger.getItems().setAll(lift.getBookinger());
+    }
+}
---
+++ SemesterproveJuni2019/src/controller/Controller.java
@@ -0,0 +1,127 @@
+package controller;
+
+import model.*;
+import storage.Storage;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class Controller {
+    /*
+     * Opretter en bil og retunerer den
+     * pre:
+     */
+    public static Bil createBil(String regNr, String chauffør) {
+        Bil bil = new Bil(regNr, chauffør);
+        Storage.storeBil(bil);
+        return bil;
+    }
+
+    /*
+     * Opretter et lift og retunerer det
+     * pre:
+     */
+    public static Lift createLift(LocalDateTime tidspunkt, int pris, String fraAdresse, String tilAdresse, int maxAntalPassagerer, Bil bil) {
+        Lift lift = new Lift(tidspunkt, pris, fraAdresse, tilAdresse, maxAntalPassagerer, bil);
+        bil.addLift(lift);
+        return lift;
+    }
+
+    /*
+     * Opretter en booking og retunerer den
+     * pre:
+     */
+    public static Booking createBooking(String passager, LocalTime opsamlingstid, String opsamlingsted, Lift lift) {
+        try {
+            if (!lift.LedigPlads()) throw new RuntimeException();
+            {
+                Booking booking = new Booking(passager, opsamlingstid, opsamlingsted, lift);
+                lift.addBooking(booking);
+                return booking;
+            }
+        } catch (RuntimeException ex) {
+            System.out.println("IKKE FLERE LEDIGE PLADSER!");
+            throw ex;
+        }
+    }
+
+    /*
+     * Opretter en anbefaling og retunerer den
+     * pre:
+     */
+    public static Anbefaling createAnbefaling(Stjerner stjerner, String kommentar) {
+        Anbefaling anbefaling = new Anbefaling(stjerner, kommentar);
+
+        return anbefaling;
+    }
+    public static ArrayList<Lift> liftPåDato(LocalDate dato){
+        ArrayList<Lift> sammeDag = new ArrayList<>();
+        ArrayList<Lift> result = new ArrayList<>();
+        for(Bil bil : Storage.getBiler()){
+            for(Lift lift : bil.getLifts())
+                if(lift.getTidspunkt().toLocalDate().equals(dato)){
+                    if(lift.LedigPlads()) {
+                        sammeDag.add(lift);
+                    }
+                }
+        }
+
+        Collections.sort(sammeDag);
+        return sammeDag;
+    }
+    public static int[] anbefalingFrekvens(){
+        int[] result = new int[5];
+        for (Bil bil : Storage.getBiler()) {
+            for (Lift lift : bil.getLifts()) {
+                for (Booking booking : lift.getBookinger()) {
+                    if (booking.getAnbefaling() != null) {
+                        if (booking.getAnbefaling().getStjerner().equals(Stjerner.EN)) {
+                            result[0]++;
+                        }
+                        if (booking.getAnbefaling().getStjerner().equals(Stjerner.TO)) {
+                            result[1]++;
+                        }
+                        if (booking.getAnbefaling().getStjerner().equals(Stjerner.TRE)) {
+                            result[2]++;
+                        }
+                        if (booking.getAnbefaling().getStjerner().equals(Stjerner.FIRE)) {
+                            result[3]++;
+                        }
+                        if (booking.getAnbefaling().getStjerner().equals(Stjerner.FEM)) {
+                            result[4]++;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    public static String findChaufførOgAnbefaling(String passager, String opsamlingsted,LocalTime opsamlingstid){
+        String tekst = "";
+        for(Bil biler : Storage.getBiler()){
+            for(Lift lift : biler.getLifts()){
+                for(Booking booking : lift.getBookinger()){
+                    String sted = booking.getOpsamlingsted();
+                    LocalTime tid = booking.getOpsamlingstid();
+                    String fundetPassager = booking.getPassager();
+
+                    if(sted.equals(opsamlingsted)&&tid.equals(opsamlingstid)&&fundetPassager.equals(passager)){
+                        if(booking.getAnbefaling() != null){
+                            return biler.getChauffør() + " " + booking.getAnbefaling();
+                        }
+                        else {
+                            return biler.getChauffør() + " " + "Ingen anbefalinger";
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    public static void setAnbefaling(Booking booking, Anbefaling anbefaling){
+        booking.setAnbefaling(anbefaling);
+    }
+}
